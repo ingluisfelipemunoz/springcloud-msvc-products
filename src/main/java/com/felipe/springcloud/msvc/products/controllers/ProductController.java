b007144573953a91450107228ad97cd751bb81d6
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-01-03 11:28:57 -0400

    add create, update, and delete methods to ProductController

--- src/main/java/com/felipe/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/felipe/springcloud/msvc/products/controllers/ProductController.java
@@ -9,9 +9,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class ProductController {
@@ -44,4 +49,31 @@ public class ProductController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping()
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Product> productOptional = service.findById(id);
+        if (productOptional.isPresent()) {
+            this.service.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> productOptional = service.findById(id);
+        if (productOptional.isPresent()) {
+            Product productDb = productOptional.orElseThrow();
+            productDb.setName(product.getName());
+            productDb.setPrice(product.getPrice());
+            productDb.setCreatedAt(product.getCreatedAt());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.save(productDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
